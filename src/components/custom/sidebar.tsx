58cd27cbebbc80921f485bf09e97eaea8b642530
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-04 20:27:21 +0100

    style: enhance Sidebar component with hover effects and improved accessibility for navigation links

--- src/components/custom/sidebar.tsx
+++ src/components/custom/sidebar.tsx
@@ -13,10 +13,14 @@ export function Sidebar() {
         return (
           <div
             key={link.label}
-            className={`flex w-full flex-col items-center justify-center py-2 ${pathname === link.href ? 'border-r-3 border-r-[#24c0b7]' : ''}`}
+            className={`group flex w-full flex-col items-center justify-center py-2 transition-all duration-300 hover:border-r-3 hover:border-r-[#24c0b7] ${pathname === link.href ? 'border-r-3 border-r-[#24c0b7]' : ''}`}
           >
-            <Link href={link.href} className={`${pathname === link.href ? 'text-[#24c0b7]' : 'text-gray-500'}`}>
-              {<link.icon />}
+            <Link
+              href={link.href}
+              aria-label={link.label}
+              className={`flex w-full items-center justify-center transition-all duration-300 group-hover:scale-105 group-hover:text-[#24c0b7] ${pathname === link.href ? 'text-[#24c0b7]' : 'text-gray-500'}`}
+            >
+              {<link.icon size={23} />}
             </Link>
           </div>
         )
