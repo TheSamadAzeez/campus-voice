64ddcc4fe04710a626745a417ed9d1260659ed81
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-04 16:57:22 +0100

    feat: add studentLinks utility for navigation in student page

---
+++ src/utils/links.ts
@@ -0,0 +1,22 @@
+export const studentLinks = [
+  {
+    label: 'Dashboard',
+    href: '/student',
+    icon: '',
+  },
+  {
+    label: 'My Complaints',
+    href: '/student/my-complaints',
+    icon: '',
+  },
+  {
+    label: 'Submit Complaint',
+    href: '/student/submit-complaint',
+    icon: '',
+  },
+  {
+    label: 'My Profile',
+    href: '/student/my-profile',
+    icon: '',
+  },
+]
