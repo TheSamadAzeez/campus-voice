9a31b5268559009296c3e06f92be2f29ba665821
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-05 09:15:37 +0100

    feat: integrate Header component into student layout for improved structure and user experience

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,10 +1,14 @@
+import { Header } from '@/components/custom/header'
 import { Sidebar } from '@/components/custom/sidebar'
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen w-screen">
       <Sidebar />
-      <div className="flex-1 rounded-l-[3rem] bg-[#f1f5f9] p-10">{children}</div>
+      <div className="flex flex-1 flex-col gap-10 rounded-l-[3rem] bg-[#f1f5f9] p-10">
+        <Header />
+        {children}
+      </div>
     </div>
   )
 }
