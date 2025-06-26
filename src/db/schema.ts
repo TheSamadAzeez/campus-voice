cc7b74f12414bf67519f4286bed0c9edac0a33a0
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-26 16:42:32 +0100

    feat: update users and complaints table schema to use clerkUserId as primary key and adjust role enum values

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,22 +1,21 @@
 import { integer, pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  clerkUserId: varchar({ length: 255 }).primaryKey().notNull().unique(),
   email: varchar({ length: 255 }).notNull().unique(),
   firstName: varchar({ length: 255 }).notNull(),
   lastName: varchar({ length: 255 }).notNull(),
   imageUrl: varchar({ length: 255 }).notNull(),
-  role: varchar({ length: 255, enum: ['user', 'admin'] })
+  role: varchar({ length: 255, enum: ['student', 'admin'] })
     .notNull()
-    .default('user'),
-  clerkUserId: varchar({ length: 255 }).notNull().unique(),
+    .default('student'),
   createdAt: timestamp('created_at', { mode: 'string' }).notNull().defaultNow(),
   updatedAt: timestamp('updated_at', { mode: 'string' }).notNull().defaultNow(),
 })
 
 export const complaintTable = pgTable('complaints', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: integer().references(() => usersTable.id),
+  userId: varchar({ length: 255 }).references(() => usersTable.clerkUserId),
   category: varchar({
     length: 255,
     enum: ['academic', 'facility', 'administration', 'harassment', 'infrastructure', 'result', 'other'],
