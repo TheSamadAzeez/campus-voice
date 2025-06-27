cdd0131b2e6f43309c11509c70c2028ab5f948ff
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-06-27 10:44:29 +0100

    feat: add error handling and logging for user management functions in actions utility

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -3,16 +3,36 @@ import { usersTable } from '@/db/schema'
 import { eq } from 'drizzle-orm'
 
 export async function createUser(user: typeof usersTable.$inferInsert) {
-  await db.insert(usersTable).values({ ...user })
+  try {
+    await db.insert(usersTable).values({ ...user })
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
 export async function updateUser(user: typeof usersTable.$inferSelect) {
-  await db
-    .update(usersTable)
-    .set({ ...user })
-    .where(eq(usersTable.clerkUserId, user.clerkUserId))
+  try {
+    await db
+      .update(usersTable)
+      .set({ ...user })
+      .where(eq(usersTable.clerkUserId, user.clerkUserId))
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
 
 export async function deleteUser(clerkUserId: string) {
   await db.delete(usersTable).where(eq(usersTable.clerkUserId, clerkUserId))
+}
+
+export async function getUserByClerkUserId(clerkUserId: string) {
+  try {
+    const user = await db.select().from(usersTable).where(eq(usersTable.clerkUserId, clerkUserId))
+    return user[0]
+  } catch (error) {
+    console.error(error)
+    return null
+  }
 }
