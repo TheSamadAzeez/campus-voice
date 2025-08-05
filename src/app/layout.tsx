3066b9f9b4734eff63f5d3427d223cb5d4d87941
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-08-05 16:26:20 +0100

    feat: integrate Toaster component for notifications in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from '@/components/ui/sonner'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,7 +30,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {children}
+          <Toaster position="bottom-right" expand={false} richColors />
+        </body>
       </html>
     </ClerkProvider>
   )
