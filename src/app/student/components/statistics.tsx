1627be0789fe1f808ccac7bf63125d40c1c0dc26
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-08-06 07:16:55 +0100

    feat: refactor Statistics component to use COMPLAINT_CARDS for rendering and improve stat value retrieval

--- src/app/student/components/statistics.tsx
+++ src/app/student/components/statistics.tsx
@@ -1,40 +1,87 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { LucideIcon } from 'lucide-react'
+import { Activity, AlertCircle, CheckCircle2, Clock, LucideIcon } from 'lucide-react'
 
-interface ComplaintStats {
+const COMPLAINT_CARDS: CardProps[] = [
+  {
+    id: 1,
+    title: 'Total Complaints',
+
+    icon: Activity,
+    color: 'green',
+  },
+  {
+    id: 2,
+    title: 'Resolved Complaints',
+    icon: CheckCircle2,
+    color: 'blue',
+  },
+  {
+    id: 3,
+    title: 'In Review',
+    icon: Clock,
+    color: 'purple',
+  },
+  {
+    id: 4,
+    title: 'Pending Complaints',
+    icon: AlertCircle,
+    color: '#f55612',
+  },
+]
+interface CardProps {
   id: number
   title: string
-  value: number
   icon: LucideIcon
   color: string
 }
 
-export function Statistics({ stats }: { stats: ComplaintStats[] }) {
+interface ComplaintStats {
+  total: number
+  pending: number
+  resolved: number
+  inReview: number
+}
+
+export function Statistics({ stats }: { stats: ComplaintStats }) {
+  const getStatValue = (title: string): number => {
+    if (title === 'Total Complaints') {
+      return stats.total
+    } else if (title === 'Resolved Complaints') {
+      return stats.resolved
+    } else if (title === 'In Review') {
+      return stats.inReview
+    } else if (title === 'Pending Complaints') {
+      return stats.pending
+    } else {
+      return 0
+    }
+  }
+
   return (
     <div className="grid w-1/2 grid-cols-2 gap-4">
-      {stats.map((stat) =>
-        stat.title === 'Total Complaints' ? (
-          <Card key={stat.id} className="bg-[#24c0b7] text-white">
+      {COMPLAINT_CARDS.map((card) =>
+        card.title === 'Total Complaints' ? (
+          <Card key={card.id} className="bg-[#24c0b7] text-white">
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-lg font-medium">{stat.title}</CardTitle>
+              <CardTitle className="text-lg font-medium">{card.title}</CardTitle>
               <div className="rounded-2xl bg-[#f1f5f9] p-3">
-                <stat.icon color={stat.color} size={20} strokeWidth={2.5} />
+                <card.icon color={card.color} size={20} strokeWidth={2.5} />
               </div>
             </CardHeader>
             <CardContent className="flex flex-1 items-center justify-start">
-              <div className="text-3xl font-bold">{stat.value}</div>
+              <div className="text-3xl font-bold">{getStatValue(card.title)}</div>
             </CardContent>
           </Card>
         ) : (
-          <Card key={stat.id}>
+          <Card key={card.id}>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-lg font-medium">{stat.title}</CardTitle>
+              <CardTitle className="text-lg font-medium">{card.title}</CardTitle>
               <div className="rounded-2xl bg-[#f1f5f9] p-3">
-                <stat.icon color={stat.color} size={20} strokeWidth={2.5} />
+                <card.icon color={card.color} size={20} strokeWidth={2.5} />
               </div>
             </CardHeader>
             <CardContent className="flex flex-1 items-center justify-start">
-              <div className="text-3xl font-bold">{stat.value}</div>
+              <div className="text-3xl font-bold">{getStatValue(card.title)}</div>
             </CardContent>
           </Card>
         ),
