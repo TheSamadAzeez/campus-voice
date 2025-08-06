bb5a32b6852876198fb6739200da3b533c594815
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-08-06 07:34:45 +0100

    feat: update getDashboardData to limit user complaints retrieval to 3

--- src/app/student/action.ts
+++ src/app/student/action.ts
@@ -1,7 +1,7 @@
 import { getComplaintStats, getUserComplaints } from '@/utils/actions/complaints'
 
 export async function getDashboardData() {
-  const [stats, complaints] = await Promise.all([getComplaintStats(), getUserComplaints()])
+  const [stats, complaints] = await Promise.all([getComplaintStats(), getUserComplaints(3)])
 
   return {
     stats,
