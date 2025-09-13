62d31fd71908b1d1b08abde10b7707ca670efab3
TheSamadAzeez <azeezsamad2004@gmail.com>
2025-09-13 15:00:19 +0100

    feat: update logo component for improved branding and accessibility

--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -11,12 +11,13 @@ interface LogoProps {
 
 export function Logo({ className, containerClassName, variant = 'light' }: LogoProps) {
   return (
-    <Link href="/" aria-label="ACME" className={cn('inline-block', containerClassName)}>
+    <Link href="/" aria-label="Campus Voice" className={cn('inline-flex items-center gap-3', containerClassName)}>
+      <Image src="/images/lasu-logo.png" width={70} height={70} alt="LASU logo" className="rounded-sm" />
       <Image
         src="/images/logo.png"
         width={104}
         height={57}
-        alt="ACME logo"
+        alt="Campus Voice logo"
         className={cn(variant === 'light' ? 'invert' : '', className)}
       />
     </Link>
